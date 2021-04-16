a1cab982ada696b262a74b74ed66da3e14a9d60b
Rajat Nagvenker <rajatnagvenker@gmail.com>
2021-04-16 21:51:31 +0530

    Initial Commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Rajat Nagvenker on 16/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
